5c395345f837708f04fab881c1a4278a14e957cc
Jawid <41361157+infjay@users.noreply.github.com>
2022-04-13 14:54:21 +0200

    adjusted the movements for the player & categorized the code into smaller parts

--- JS/game.js
+++ JS/game.js
@@ -6,45 +6,75 @@ class Game {
 
 
     }
-    start(){
+    start() {
         this.createPlayer();
         this.createOpponent();
+        this.eventListener();
     }
- 
+
     createPlayer() {
         // Creating a player and adding it to the DOM & assign it to our game board
-        this.player = new Player(20, 10);
+        this.player = new Player();
         const newPlayer = document.createElement('div');
         newPlayer.className = 'player';
         this.board.appendChild(newPlayer);
         // Selecting the player element in the DOM & Drawing the player
+        this.drawingPlayer();
+    }
+
+    drawingPlayer() {
         const drawPlayer = document.querySelector('.player');
         drawPlayer.style.width = "100px"
         drawPlayer.style.height = "200px"
+        drawPlayer.style.left = this.player.positionX + "vw"
+        drawPlayer.style.bottom = this.player.positionY + "vh"
+    }
+
+    drawingOpponent(){
+        const drawOpp = document.querySelector('.opponent');
+        drawOpp.style.width = "100px"
+        drawOpp.style.height = "200px"
+        drawOpp.style.left = this.opponents.positionX + "vw"
+        drawOpp.style.top = this.opponents.positionY + "vh"
+    }
+
+    createOpponent() {
+        setInterval(() => {
+            // Creating new Opponents and Putting them into Array every 1 Seconds
+            let newOpp = new Opponent()
+            this.opponents.push(newOpp)
+            let newOpponent = document.createElement('div');
+            newOpponent.className = 'opponent';
+            this.board.appendChild(newOpponent);
+            this.drawingOpponent();
+        }, 1000);
+
 
     }
+
+    eventListener() {
+        console.log(this.player.positionX);
+        document.addEventListener("keydown", (event) => {
+            // assigning keys to movement
+            if (event.key === "ArrowRight") {
+                 this.player.steerRight();
+            } else if (event.key === "ArrowLeft") {
+                this.player.steerLeft();
+                console.log("move left");
+            } 
+           this.drawingPlayer();
     
+        });
+    }
 
-     createOpponent() {
-         setInterval(() => {
-             // Creating new Opponents and Putting them into Array every 1 Seconds
-             let newOpp = new Opponent()
-            this.opponents.push(newOpp)
-             let newOpponent = document.createElement('div');
-             newOpponent.className = 'opponent';
-             this.board.appendChild(newOpponent);
-             const drawOpp = document.querySelector('.opponent');
-             drawOpp.style.width = "100px"
-             drawOpp.style.height = "200px"
-            moveDown();
-         }, 1000);
-
-       
-     }
-
-    
     
 }
+
+
+
+    
+    
+
 
 
 
@@ -57,10 +87,10 @@ class Player {
         this.positionY = 0;
     }
     steerRight() {
-        this.positionX += 1;
+        this.positionX += 3;
     }
-    steerRight() {
-        this.positionY -= 1;
+    steerLeft() {
+        this.positionX -= 3;
     };
 }
 
@@ -68,12 +98,12 @@ class Player {
 class Opponent extends Player {
     constructor(height, width) {
         super(height, width);
-        this.positionX = 0;
-        this.positionY = Math.random() * 90;
+        this.positionX = Math.random() * 90;
+        this.positionY = 0;
 
     }
     moveDown() {
-        this.positionY -= 1;
+        this.positionY -=1;
     }
 }
 
